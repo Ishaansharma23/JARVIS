8ef347f90dfa121884c980dad33c128bfe3a8975
Ishaan Sharma <sharmaishaan0009@gmail.com>
2025-07-20 20:03:48 +0530

    Added API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-
+import jarvis from './assets/giphy.gif'
+import { FaMicrophoneAlt } from "react-icons/fa";
 
 const App = () => {
   return (
-    <div className='h-screen w-full bg-black font-[Nunito_Sans] text-white'>
-      <div className='img'>
-        <img src={jarvis} />
+    <div className='h-screen w-full bg-black font-[Nunito_Sans] text-white flex justify-center items-center flex-col'>
+      <div className='img -mt-[250px]'>
+        <img src={jarvis} alt="Jarvis" />
       </div>
-      <div></div>
+      <div className='text flex flex-col items-center'>
+        <h1 className='text-4xl font-bold bg-gradient-to-r from-white  to-blue-300 bg-clip-text text-transparent mb-4'>Hi, I'm JARVIS</h1>
+        <button className='border-2 border-zinc-700 bg-blue-300 text-black text-2xl rounded-4xl px-5 py-1 flex justify-center items-center gap-1 -ml-2 shadow-lg shadow-cyan-400/90'>
+          Click Here <FaMicrophoneAlt />
+        </button>
+      </div>
     </div>
   )
 }
