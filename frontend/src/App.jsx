ff1ed36328be52e597b6f9b5973306c3608a58ec
Ishaan Sharma <sharmaishaan0009@gmail.com>
2025-07-20 19:00:47 +0530

    initialized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-black font-[Nunito_Sans] text-white'>
+      <div className='img'>
+        <img src={jarvis} />
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default App
